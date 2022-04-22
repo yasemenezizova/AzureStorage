0a23bc3833d057fd8cab019a586fa6613903d776
Yasemen Azizova <yasemen.a@prosys.local>
2022-04-22 14:06:44 +0400

    azure queue storage

---
+++ AzureStorageLibrary/Services/AzQueueStorage.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageLibrary.Services
+{
+    public class AzQueueStorage
+    {
+        private readonly QueueClient _queueClient;
+
+        public AzQueueStorage(string queueName)
+        {
+            _queueClient = new QueueClient(ConnectionStrings.AzureStorageConnectionString, queueName);
+            _queueClient.CreateIfNotExists();
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            await _queueClient.SendMessageAsync(message);
+        }
+
+        public async Task<QueueMessage> RetrieveNextMessageAsync()
+        {
+            QueueProperties properties = await _queueClient.GetPropertiesAsync();
+            if (properties.ApproximateMessagesCount>0)
+            {
+                QueueMessage[] queueMessage = await _queueClient.ReceiveMessagesAsync(1, TimeSpan.FromMinutes(1));
+                if (queueMessage.Any())
+                {
+                    return queueMessage[0];
+                }
+            }
+            return null;
+        }
+
+        public async Task DeleteMessageAsync(string messageId, string popReceipt)
+        {
+            await _queueClient.DeleteMessageAsync(messageId, popReceipt);
+        }
+    }
+}
---
+++ QueueConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using AzureStorageLibrary.Services;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueConsoleApp
+{
+    internal class Program
+    {
+        private async static Task Main(string[] args)
+        {
+            AzureStorageLibrary.ConnectionStrings.AzureStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=projectbyasaman;AccountKey=/i3ZOXxgBMGCCE8uX7p1FH3H3Qmd+RgZuIeiWk1M3bRRkJ4E4JRzCR/YpWqzJ0xPI/RZIHcEiI3v+AStEtfg1A==;BlobEndpoint=https://projectbyasaman.blob.core.windows.net/;QueueEndpoint=https://projectbyasaman.queue.core.windows.net/;TableEndpoint=https://projectbyasaman.table.core.windows.net/;FileEndpoint=https://projectbyasaman.file.core.windows.net/;";
+            AzQueueStorage queue = new AzQueueStorage("examplequeue");
+
+            #region SendingMessage
+            string base64Message = Convert.ToBase64String(Encoding.UTF8.GetBytes("salam Yasemen Azizova"));
+            queue.SendMessageAsync(base64Message).Wait();
+            Console.WriteLine("sending process was succesfully");
+            #endregion
+
+            #region ReadingMessage
+            var message = queue.RetrieveNextMessageAsync().Result;
+
+            string text = Encoding.UTF8.GetString(Convert.FromBase64String(message.MessageText));
+
+            Console.WriteLine("Message: " + text);
+            #endregion
+
+            #region DeletingMessage
+            await queue.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+            Console.WriteLine("Message was deleted");
+            #endregion
+
+        }
+    }
+}
